f99428fca5036889fcecf2fa10b6e6590e328c8b
Falk Hoppe <falkhoppe81@gmail.com>
2015-02-26 11:23:52 +0100

    added creation resource

--- swarm_factory.go
+++ swarm_factory.go
@@ -1,14 +1,33 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
+)
+
+// User is a new swarm family member
+type User struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
 // AppEngine for web engine setup
 func AppEngine() *martini.ClassicMartini {
 	m := martini.Classic()
+
+	m.Post("/", binding.Json(User{}), func(user User, resp http.ResponseWriter) {
+		fmt.Sprintf("%s : %s", user.Email, user.Password)
+
+		resp.WriteHeader(http.StatusCreated)
+	})
+
 	return m
 }
 
 func main() {
 	m := AppEngine()
-	m.Run()
+	m.RunOnAddr(":9001")
 }
